aa435b19b51f022ddf9ffc3415026cc4321812d6
Yatin Gupta <guptayatin4@gmail.com>
2023-06-15 07:01:00 -0500

    Made the UI look nicer, added a pop up, logo, name, and improved zooming and other features

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+// import './App.css';
+// import Graph from './components/Graph';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Graph />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
 import Graph from './components/Graph';
+import { BrowserRouter as Router } from 'react-router-dom';
+import logo from './state-farm-insurance-vector-logo.png';
+import SearchBar from "./components/SearchBar/SearchBar";
+import BookData from "./components/SearchBar/Data.json";
+import TableCell from "@mui/material/TableCell";
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <div className="top">
+        <img src={logo} className = "navbar-logo" alt='logo' />
+        Nexus
+      </div>
+      <TableCell component="th" scope="row">
+        <SearchBar placeholder="Enter Name" data={BookData} />
+      </TableCell>
       <Graph />
-    </div>
+    </>
   );
 }
 
